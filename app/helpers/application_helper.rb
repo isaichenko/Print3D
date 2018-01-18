7f9f6a880f2ecf251ad80d7b5a41b0925727431c
isaichenko <isaichenko.e@gmail.com>
2018-01-18 11:33:51 +0200

    Google Analitics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Фабрика Технологй"
+    base_title = "Фабрика Технологий"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
   end
- 
-end  
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 20180107133617) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
