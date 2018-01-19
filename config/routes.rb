bfd7ccb3ec2dab7d6986caddcebfa3d22806b327
isaichenko <clashinua@gmail.com>
2018-01-19 23:53:18 +0200

    Services

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get    '/your_model', to: 'stl_model#your_model'
   get    '/about',      to: 'static_pages#about'
   get    '/contact',    to: 'static_pages#contact'
+  get    '/services',   to: 'static_pages#services'
   get    '/signup', 	  to: 'users#new'
   get    '/login',      to: 'sessions#new'
   post   '/login',      to: 'sessions#create'
