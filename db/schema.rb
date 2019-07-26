215a9f57496e8b602591ab05174a050c3e179a81
isaichenko <isaichenko.e@gmail.com>
2019-07-27 01:42:25 +0300

    add price file

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20180130135441) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_link"
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 20180130135441) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
